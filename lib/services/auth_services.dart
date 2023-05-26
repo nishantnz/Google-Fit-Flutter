32dca6c3c460b665cfdb9b88153d6584177b9815
nishantnz <nishant.z@somaiya.edu>
2023-05-26 22:13:28 +0530

    signInWithGoogle

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,7 +1,26 @@
+import 'dart:html';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class AuthService {
   //Google Sign In
 
   signInWithGoogle() async {
     //begin interactive sihn in process
+
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    //obtain auth details from the request
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    //create a new credential for user
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    //finally lets sign in the user
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
